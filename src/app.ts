4ac63498cda4a94fd4e23fe4d1a11418ca316501
vitorbelarmino <vitor.belarmino@hotmail.com>
2023-12-27 00:17:19 -0300

    feat: add CORS

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,23 @@ export class App {
   public app: express.Application;
   constructor() {
     this.app = express();
+    this.config();
     this.app.use(express.json());
     this.app.use(router);
     this.app.use(globalError);
+  }
+
+  private config(): void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+    this.app.use(accessControl);
   }
 
   public start(port: number | string) {
